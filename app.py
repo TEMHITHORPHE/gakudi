16a345ee32bb12fa47dc8a41f93ca8e79d51d115
JTMax1 <johntochukwumax@gmail.com>
2022-04-10 05:09:38 +0100

    Added error handling pages

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from werkzeug import exceptions as Err
 
 app = Flask(__name__)
+
+
+@app.errorhandler(Err.NotFound)
+def handle_bad_request(e):
+    return 'Page not found!', 404
+
+# or, without the decorator
+# app.register_error_handler(400, handle_bad_request);
+
 
 @app.route("/")
 def hello_world():
@@ -9,4 +19,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(port=5000)
-    
